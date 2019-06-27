3309facee84a27ae5d052b494301a4f7f8693df1
NikitaElmanov <nelmanov8@gmail.com>
2019-06-27 21:09:58 +0300

    add some changes at finish

--- SequenceOfTask/src/main/java/ru/sber/seq/tasks/steps/Step.java
+++ SequenceOfTask/src/main/java/ru/sber/seq/tasks/steps/Step.java
@@ -14,32 +14,43 @@ import java.util.Objects;
 public class Step implements Serializable {
     private Integer number;
     private String command;
+    private Boolean checkPreviousStepRes;
     private Boolean isDone;
+    private Boolean hasError;
 
     private List<Integer> parallelWith;
     private List<Integer> goThen;
+    private List<Integer> previousSteps;
 
     private Step(StepBuilder builder){
         this.number = builder.number;
         this.command = builder.command;
+        this.checkPreviousStepRes = builder.checkPreviousStepRes;
         this.isDone = builder.isDone;
+        this.hasError = builder.hasError;
 
         this.parallelWith = builder.parallelWith;
         this.goThen = builder.goThen;
+        this.previousSteps = builder.previousSteps;
     }
 
     public static class StepBuilder{
         private Integer number;
         private String command;
+        private Boolean checkPreviousStepRes;
         private Boolean isDone;
+        private Boolean hasError;
 
         private List<Integer> parallelWith;
         private List<Integer> goThen;
+        private List<Integer> previousSteps;
 
-        public StepBuilder(Integer number, String command, Boolean isDone){
+        public StepBuilder(Integer number, String command, Boolean checkPreviousStepRes, Boolean hasError){
             this.number = number;
             this.command = command;
-            this.isDone = isDone;
+            this.checkPreviousStepRes = checkPreviousStepRes;
+            this.hasError = hasError;
+            this.isDone = false;
         }
 
         public StepBuilder setParallelWith(List<Integer> parallelWith) {
@@ -52,6 +63,11 @@ public class Step implements Serializable {
             return this;
         }
 
+        public StepBuilder setPreviousSteps(List<Integer> previousSteps) {
+            this.previousSteps = previousSteps;
+            return this;
+        }
+
         public Step build(){
             return new Step(this);
         }
@@ -59,16 +75,14 @@ public class Step implements Serializable {
 
     public static void doSome(List<Step> steps) {
 
-        for (Step step : steps){
+        for (Step step : steps) {
 
             if (!step.getIsDone()) {
 
-                System.out.println(step.getCommand());
-                step.setIsDone(true);
+                if (Objects.nonNull(step.getParallelWith())) {
+                    for (Integer num : step.getParallelWith()) {
+                        if (!steps.get(num).getIsDone()) {
 
-                if (Objects.nonNull(step.getParallelWith())){
-                    for (Integer num : step.getParallelWith()){
-                        if (!steps.get(num).getIsDone()){
                             System.out.println(steps.get(num).getCommand());
                             steps.get(num).setIsDone(true);
                         }
@@ -76,6 +90,10 @@ public class Step implements Serializable {
                 }
 
                 if (Objects.nonNull(step.getGoThen())) {
+
+                    System.out.println(step.getCommand());
+                    step.setIsDone(true);
+
                     for (Integer num : step.getGoThen()) {
                         if (!steps.get(num).getIsDone()) {
 
--- SequenceOfTask/src/main/java/ru/sber/seq/tasks/unimportant/MainIntoFile.java
+++ SequenceOfTask/src/main/java/ru/sber/seq/tasks/unimportant/MainIntoFile.java
@@ -14,36 +14,42 @@ public class MainIntoFile {
         List<Step> steps = new ArrayList<>();
 
         steps.add(
-                new Step.StepBuilder(0, "This is Step 0", false)
+                new Step.StepBuilder(0, "This is Step 0", false, true)
                         .setGoThen(Arrays.asList(1, 2, 6))
                         .build());
         steps.add(
-                new Step.StepBuilder(1, "This is Step 1", false)
+                new Step.StepBuilder(1, "This is Step 1", true, false)
                         .setGoThen(Arrays.asList(3))
                         .setParallelWith(Arrays.asList(2, 6))
+                        .setPreviousSteps(Arrays.asList(0))
                         .build());
         steps.add(
-                new Step.StepBuilder(2, "This is Step 2", false)
+                new Step.StepBuilder(2, "This is Step 2", false, false)
                         .setGoThen(Arrays.asList(4))
                         .setParallelWith(Arrays.asList(1, 6))
+                        .setPreviousSteps(Arrays.asList(0))
                         .build());
         steps.add(
-                new Step.StepBuilder(3, "This is Step 3", false)
+                new Step.StepBuilder(3, "This is Step 3", false, false)
                         .setGoThen(Arrays.asList(5))
                         .setParallelWith(Arrays.asList(4))
+                        .setPreviousSteps(Arrays.asList(1, 6))
                         .build());
         steps.add(
-                new Step.StepBuilder(4,"This is Step 4", false)
+                new Step.StepBuilder(4,"This is Step 4", false, false)
                         .setGoThen(Arrays.asList(5))
                         .setParallelWith(Arrays.asList(3))
+                        .setPreviousSteps(Arrays.asList(2))
                         .build());
         steps.add(
-                new Step.StepBuilder(5,"This is Step 5", false)
+                new Step.StepBuilder(5,"This is Step 5", false, false)
+                        .setPreviousSteps(Arrays.asList(3, 4))
                         .build());
         steps.add(
-                new Step.StepBuilder(6, "This is Step 6", false)
+                new Step.StepBuilder(6, "This is Step 6", false, false)
                         .setGoThen(Arrays.asList(3))
                         .setParallelWith(Arrays.asList(1, 2))
+                        .setPreviousSteps(Arrays.asList(0))
                         .build());
 
         ConvertMapper.convertInto(steps);
