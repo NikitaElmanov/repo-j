79b98e66a5ae5161cb89982e203c9dcd304022db
NikitaElmanov <nelmanov8@gmail.com>
2019-09-01 11:50:26 +0300

    add some changes like 'test'

--- ServerHandler/src/main/java/ru/serv/pro/servlets/Frontend.java
+++ ServerHandler/src/main/java/ru/serv/pro/servlets/Frontend.java
@@ -10,6 +10,8 @@ public class Frontend extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+
+        System.out.println("sdvdv");
     }
 
     @Override
