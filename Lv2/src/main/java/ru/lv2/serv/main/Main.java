14110a301823220542888f47ceb0f3008c7b1521
NikitaElmanov <nelmanov8@gmail.com>
2019-09-14 11:44:58 +0300

    created new web server project with several servlets and supported classes

---
+++ Lv2/src/main/java/ru/lv2/serv/main/Main.java
@@ -0,0 +1,38 @@
+package ru.lv2.serv.main;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import ru.lv2.serv.accounts.AccountService;
+import ru.lv2.serv.accounts.UserProfile;
+import ru.lv2.serv.servlets.SessionsServlet;
+import ru.lv2.serv.servlets.UsersServlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(8080);
+        server.setHandler(handlers);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+    }
+}
---
+++ Lv2/src/main/java/ru/lv2/serv/servlets/SessionsServlet.java
@@ -0,0 +1,84 @@
+package ru.lv2.serv.servlets;
+
+import com.google.gson.Gson;
+import ru.lv2.serv.accounts.AccountService;
+import ru.lv2.serv.accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+public class SessionsServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SessionsServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+
+    //authorizing (sign up)
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String pass = req.getParameter("pass");
+
+        if (login == null || pass == null) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile userProfile = accountService.getUserByLogin(login);
+        if (userProfile == null || !userProfile.getPass().equals(pass)) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        accountService.addSession(req.getSession().getId(), userProfile);
+        Gson gson = new Gson();
+        String json = gson.toJson(userProfile);
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println(json);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+
+    //sign in
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserProfile userProfile = accountService.getUserBySessionId(req.getSession().getId());
+
+        if (Objects.isNull(userProfile)){
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(userProfile);
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println(json);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    //sign out
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String sessionId = req.getSession().getId();
+        UserProfile userProfile = accountService.getUserBySessionId(sessionId);
+
+        if (Objects.isNull(userProfile)){
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            accountService.deleteSession(sessionId);
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().println("GoodBye!");
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+}
