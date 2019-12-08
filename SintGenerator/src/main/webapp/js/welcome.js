c4309fa566721a6e590d947423ee996bb53fad8a
NikitaElmanov <nelmanov8@gmail.com>
2019-12-08 19:41:58 +0300

    added main interactive logic in js and changed styles of pages

--- SintGenerator/src/main/webapp/js/welcome.js
+++ SintGenerator/src/main/webapp/js/welcome.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     var counterOfRows = 0;
     var maxAmountOfColumns = 15;
-    var stepOfExpandingMW = 35;
+    var stepOfExpandingMW = 30;
+    var pkTypes = ['INT','UNSIGNED INT'];
+    var precTypes = ['CHAR','VARCHAR'];
+    var noPrecTypes = ['BOOLEAN','INT','UNSIGNED INT', 'DATE'];
+    var typesFields, precisionFields, pkFields;
 
     $('input#addRow').click(function(){//adding new row (column) for table
         if (counterOfRows <= maxAmountOfColumns){
@@ -17,17 +21,21 @@ $(document).ready(function() {
             }
 
             $('.table-tbody').append('<ul class="table-tr tr-body">'+
-                '<li class="table-td"><input type="text"/></li>'+
+                '<li class="table-td"><input type="text" class="field-name"/></li>'+
                 '<li class="table-td">'+
                 '<select class="type">'+
-                '<option selected>The first option</option>'+
-                '<option>The second option</option>'+
-                '<option>The third option</option>'+
+                '<option selected>VARCHAR</option>'+
+                '<option>CHAR</option>'+
+                '<option>INT</option>'+
+                '<option>UNSIGNED INT</option>'+
+                '<option>DECIMAL</option>'+
+                '<option>DATE</option>'+
+                '<option>BOOLEAN</option>'+
                 '</select>'+
                 '</li>'+
-                '<li class="table-td"><input type="text" id="field-name"	/></li>'+
+                '<li class="table-td"><input type="text" class="precision"/></li>'+
                 '<li class="table-td">'+
-                '<input type="checkbox" class="pk" name="pk"/>'+
+                '<input type="radio" class="pk" name="pk" style="visibility:hidden"/>'+
                 '</li>'+
                 '<li class="table-td">'+
                 '<input type="checkbox" class="del" name="del"/>'+
@@ -41,65 +49,175 @@ $(document).ready(function() {
         }
     });
 
-    var generateBtn = $('#generate');
+    window.setInterval(function () {
+        precisionFields = $('input.precision');
+        typesFields = $('select.type');
+        pkFields = $('input.pk');
+        var activeFlag = 0;
+
+        //handling types {if type = 'BOOLEAN','INT','UNSIGNED INT' then disable}
+        for (var i = 0; i < typesFields.length; i++){
+            var activeFlag = 0;
+
+            for (var k = 0; k < noPrecTypes.length; k++){
+                //'BOOLEAN','INT','UNSIGNED INT', 'DATE' !!!date is here temporarily, probably
+                if (noPrecTypes[k] == typesFields[i].value){
+                    activeFlag = 1;
+                }
+            }
+
+            if (activeFlag == 1){
+                precisionFields[i].removeAttribute('placeholder');
+                precisionFields[i].style.background = 'white';
+                precisionFields[i].setAttribute('disabled', 'true');
+            } else {
+                precisionFields[i].removeAttribute('disabled');
+            }
+
+            if ('DECIMAL' == typesFields[i].value){
+                precisionFields[i].style.background = 'white';
+                precisionFields[i].setAttribute('placeholder', '5.2');
+            }
+        }
+
+        //-----------------------------------------------
+        //handling difficult condition enough (PK visibility and checking)
+        var disPKFlag = false;
+        var index = 0;
+        var counter = 0;
+
+        for (var i = 0; i < typesFields.length; i++){
+
+            for (var k = 0; k < pkTypes.length; k++){
+                if (pkTypes[k] == typesFields[i].value){
+                    disPKFlag = true;
+                    index = i;
+                } else {
+                    pkFields[i].style.visibility = 'hidden';
+                }
+            }
+        }
+
+        pkFields.each(function(){
+            if ($(this).is(':checked')){
+                counter++;
+            }
+        });
+
+        if (disPKFlag && counter <= 1){
+            pkFields[index].style.visibility = 'visible';
+        }
+
+        pkFields.each(function(){
+            if ($(this).css('visibility') == 'hidden'){
+                $(this).prop('checked', false);
+            }
+        });
+        //---------------------------------------------------
+
+    }, 500);
+
+    $('#del').click(function(){
+        var delCheckBoxs = $('input.del');
+        var amountOfChecked = 0;
+        var originalHeight = $('.modal-w').height();
+        var newHeight;
+
+        delCheckBoxs.each(function(){
+            if ($(this).is(':checked')){
+                amountOfChecked++;
+            }
+        });
+
+        if ($('input.del').length == amountOfChecked){
+            alert('one column (filed) has to retain');
+        } else {
+            originalHeight -= stepOfExpandingMW*amountOfChecked;
+            $('.modal-w').css("height", originalHeight);
+
+            delCheckBoxs.each(function(){//if amountOfChecked = 0 then happens nothing
+                if ($(this).is(':checked')){
+                    $(this).parents('.table-tr').remove();
+                }
+            });
+        }
+    });
+
+    var allGoodFlag = 1;
+    var generateBtn = $('#generate');//running generate process
     generateBtn.click(function(){//handling of empty filed name error
-        $('li.table-td input[type="text"]').each(function(){
+        $('li.table-td input.field-name').each(function(){
             if ($.trim($( this ).val()).length == 0 || !$.trim($( this ).val()).match('[a-zA-Z][0-9]{0,}')){
                 $( this ).css('border', '2px solid red');
                 $( this ).val('');
                 $( this ).attr('placeholder', 'input field name!');
+
+                allGoodFlag = 0;
             } else {
                 $( this ).css('border', '1px solid darkblue');
             }
         });
-    });
+        //-----------------------------
 
-    window.setInterval(function () {
-        var checkBoxPK = $('input.pk');
-        var checkedFlag = true;
+        for (var j = 0; j < typesFields.length; j++){
+            for (var i = 0; i < precTypes.length; i++){
+                if (precTypes[i] == typesFields[j].value){//'CHAR','VARCHAR'
+                    if (precisionFields[j].value.match('^\\d{1,3}$')){
+                        precisionFields[j].removeAttribute('placeholder');
+                        precisionFields[j].style.background = 'white';
+                    } else {
+                        precisionFields[j].value = "";
+                        precisionFields[j].setAttribute('placeholder','input figure');
+                        precisionFields[j].style.background = 'pink';
 
-        checkBoxPK.each(function(){
-            if ($(this).is(':checked')){
-                checkedFlag = false;
-
-                checkBoxPK.each(function(){
-
-                    if ($(this).attr('disabled') == undefined) {
-                        $(this).attr('disabled', 'disabled');
+                        allGoodFlag = 0;
                     }
-                });
-
-                $(this).removeAttr('disabled');
-            }
-
-            if (checkedFlag){
-                checkBoxPK.each(function(){
-                    $(this).removeAttr('disabled');
-                });
-            }
-        });
-    }, 500);
-
-    $('#del').click(function(){
-        if ($('input.del').length > 1){
-            var delCheckBoxs = $('input.del');
-            var amountOfChecked = 0;
-
-            delCheckBoxs.each(function(){
-                if ($(this).is(':checked')){
-                    amountOfChecked++;
                 }
-            });
-
-            if ($('input.del').length == amountOfChecked){
-                alert('one column (filed) has to retain');
-            } else {
-                delCheckBoxs.each(function(){//if amountOfChecked = 0 then happens nothing
-                    if ($(this).is(':checked')){
-                        $(this).parents('.table-tr').remove();
-                    }
-                });
             }
+
+            if ('DECIMAL' == typesFields[j].value){//decimal
+                if (precisionFields[j].value.match('^\\d{1}$') || precisionFields[j].value.match('^\\d{1}((?=[.|,]\\d{0,1})|[.|,]\\d{0,1})$')){
+                    precisionFields[j].removeAttribute('placeholder');
+                    precisionFields[j].style.background = 'white';
+                } else {
+                    precisionFields[j].value = "";
+                    precisionFields[j].setAttribute('placeholder','input figure');
+                    precisionFields[j].style.background = 'pink';
+
+                    allGoodFlag = 0;
+                }
+            }
+        }
+
+        if (allGoodFlag == 1){
+            var insertScript = $("input#insert");
+            var updateScript = $("input#update");
+            var addCreateScript = $("input#add-create-script");
+            var addIdScript = $("input#addId");
+
+            var resParams = '';
+
+            if (insertScript.is(':checked')){
+                resParams += "insert;";
+            }
+            if (updateScript.is(':checked')){
+                resParams += "update;";
+            }
+            if (addCreateScript.is(':checked')){
+                resParams += "create;";
+            }
+            if (addIdScript.is(':checked')){
+                resParams += "id;";
+            }
+
+            var fieldNames = $('input.field-name');
+            var fieldTypes = $('select.type');
+            var fieldPrecisions = $('input.precision');
+
+            var fieldPK = $('input.pk');
+            var idOfPKField;
+            console.log(fieldPK.length);
+
         }
     });
 });
--- SintGenerator/src/main/webapp/view/welcome.jsp
+++ SintGenerator/src/main/webapp/view/welcome.jsp
@@ -13,6 +13,7 @@
     %>
 
     <c:if test="${username != null}">
+    <div id="username-container"><h1>${username}</h1></div>
     <form action="/logout" method="post" class="welcome-shift">
         <input type="submit" value="Logout"/>
     </form>
@@ -34,18 +35,22 @@
                         </ul>
                     </div>
                     <div class="table-tbody">
-                        <ul class="table-tr tr-body">
-                            <li class="table-td"><input type="text" id="field-name"	/></li>
+                        <ul class="table-tr tr-body" id='first-row-table-body'>
+                            <li class="table-td"><input type="text" class="field-name"/></li>
                             <li class="table-td">
                                 <select class="type">//loop set values of types
-                                    <option selected>The first option</option>
-                                    <option>The second option</option>
-                                    <option>The third option</option>
+                                    <option selected>VARCHAR</option>
+                                    <option>CHAR</option>
+                                    <option>INT</option>
+                                    <option>UNSIGNED INT</option>
+                                    <option>DECIMAL</option>
+                                    <option>DATE</option>
+                                    <option>BOOLEAN</option>
                                 </select>
                             </li>
-                            <li class="table-td"><input type="text" id="precision"/></li>
+                            <li class="table-td"><input type="text" class="precision"/></li>
                             <li class="table-td">
-                                <input type="checkbox" class="pk" name="pk"/>
+                                <input type="radio" class="pk" name="pk" style="visibility:hidden"/>
                             </li>
                             <li class="table-td">
                                 <input type="checkbox" class="del" name="del"/>
@@ -58,9 +63,22 @@
             </div>
             <div class="modal-w-row">
                 <div id="finish-attrs">
-                    <div name="insert"><input type="checkbox" name="insert" value="insert"/>insert</div>
-                    <div name="addCS"><input type="checkbox" name="addCS" value="addCS"/>Add creating script</div>
-                    <div name="addId"><input type="checkbox" name="addId" value="addId"/>Add id autoincrement</div>
+                    <div name="insert-script" id="insert-script">
+                        <input id="insert" type="radio" name="script" checked/>
+                        <label for="insert">Insert script</label>
+                    </div>
+                    <div name="update-script" id="update-script">
+                        <input id="update" type="radio" name="script"/>
+                        <label for="update">Update script</label>
+                    </div>
+                    <div name="add-create-script">
+                        <input type="checkbox" name="addCS" value="addCS" id="add-create-script"/>
+                        <label for="add-create-script">Add creating script</label>
+                    </div>
+                    <div name="addId">
+                        <input type="checkbox" name="addId" value="addId" id="addId"/>
+                        <label for="addId">Add id autoincrement</label>
+                    </div>
                 </div>
                 <!--<form action="" method="get" id="generate">-->
                 <button type="submit" value="Generate" id="generate">Generate</button>
