098328176fe2791aa6f35ecf2f0005f5acf163f4
NikitaElmanov <nelmanov8@gmail.com>
2020-03-31 19:04:28 +0300

    renamed download sql file servlet class

--- SintGenerator/src/main/java/ru/web/app/controller/DownloadSQLFileServlet.java
+++ SintGenerator/src/main/java/ru/web/app/controller/DownloadSQLFileServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 @WebServlet(urlPatterns = "/downloadSQL")
-public class DownloadSQLFile extends HttpServlet {
+public class DownloadSQLFileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
