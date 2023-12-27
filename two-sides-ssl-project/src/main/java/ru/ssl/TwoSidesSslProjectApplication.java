55677a1cc9d99e52700db258702d9819e521da5c
Nikita <39399744+NikitaElmanov@users.noreply.github.com>
2023-12-27 11:40:35 +0300

    first commit

---
+++ two-sides-ssl-project/src/main/java/ru/ssl/TwoSidesSslProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoSidesSslProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoSidesSslProjectApplication.class, args);
+    }
+
+}
